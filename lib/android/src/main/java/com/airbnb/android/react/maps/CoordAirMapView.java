3d9c76e72f70caec9b9e442bb6cb164d6fafa8a4
domenicogabriele <dommy.g1985@gmail.com>
2019-06-28 11:30:12 +0200

    Fix Formatting

--- lib/android/src/main/java/com/airbnb/android/react/maps/CoordAirMapView.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/CoordAirMapView.java
@@ -13,68 +13,68 @@ import com.facebook.react.uimanager.ThemedReactContext;
 import com.hardsoftstudio.widget.AnchorSheetBehavior;
 
 public class CoordAirMapView extends LinearLayout {
-  public CoordAirMapManager manager;
-  public View floatingActionButton;
-  public AirMapView airMapView;
+    public CoordAirMapManager manager;
+    public View floatingActionButton;
+    public AirMapView airMapView;
 
-  private AnchorSheetBehavior<FrameLayout> mainBottomSheetBehavior;
+    private AnchorSheetBehavior<FrameLayout> mainBottomSheetBehavior;
 
-  public CoordAirMapView(Context context, CoordAirMapManager coordAirMapManager) {
-    super(context);
-    this.manager = coordAirMapManager;
-    init();
-  }
+    public CoordAirMapView(Context context, CoordAirMapManager coordAirMapManager) {
+        super(context);
+        this.manager = coordAirMapManager;
+        init();
+    }
 
-  private void init() {
-    View view = View.inflate(getContext(), R.layout.coordinator, this);
-    FrameLayout replaceSheet = view.findViewById(R.id.replaceSheet);
-    floatingActionButton = view.findViewById(R.id.floatingPoint);
-    mainBottomSheetBehavior = AnchorSheetBehavior.from(replaceSheet);
-    mainBottomSheetBehavior.setAnchorOffset(0.25f);
-    final CoordAirMapView coordAirMapView=this;
-    mainBottomSheetBehavior.setAnchorSheetCallback(new AnchorSheetBehavior.AnchorSheetCallback() {
-      @Override
-      public void onStateChanged(@NonNull View bottomSheet, int newState) {
+    private void init() {
+        View view = View.inflate(getContext(), R.layout.coordinator, this);
+        FrameLayout replaceSheet = view.findViewById(R.id.replaceSheet);
+        floatingActionButton = view.findViewById(R.id.floatingPoint);
+        mainBottomSheetBehavior = AnchorSheetBehavior.from(replaceSheet);
+        mainBottomSheetBehavior.setAnchorOffset(0.25f);
+        final CoordAirMapView coordAirMapView = this;
+        mainBottomSheetBehavior.setAnchorSheetCallback(new AnchorSheetBehavior.AnchorSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
 
-        if (BottomSheetBehavior.STATE_DRAGGING == newState) {
-          floatingActionButton.animate().scaleX(1).scaleY(1).setDuration(300).start();
-        } else if (BottomSheetBehavior.STATE_EXPANDED == newState) {
-          floatingActionButton.animate().scaleX(0).scaleY(0).setDuration(300).start();
-        }
+                if (BottomSheetBehavior.STATE_DRAGGING == newState) {
+                    floatingActionButton.animate().scaleX(1).scaleY(1).setDuration(300).start();
+                } else if (BottomSheetBehavior.STATE_EXPANDED == newState) {
+                    floatingActionButton.animate().scaleX(0).scaleY(0).setDuration(300).start();
+                }
 //        //Send Event to Javascript
-        if(CoordAirMapManager.stateConvert.containsKey(newState)) {
-          WritableMap event = new WritableNativeMap();
-          event.putString("status", CoordAirMapManager.stateConvert.get(newState));
-          manager.pushEvent((ThemedReactContext) getContext(), coordAirMapView, "newStatusValue", event);
-        }
-      }
+                if (CoordAirMapManager.stateConvert.containsKey(newState)) {
+                    WritableMap event = new WritableNativeMap();
+                    event.putString("status", CoordAirMapManager.stateConvert.get(newState));
+                    manager.pushEvent((ThemedReactContext) getContext(), coordAirMapView, "newStatusValue", event);
+                }
+            }
 
-      @Override
-      public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-      }
-    });
-    floatingActionButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        airMapView.centerToUserLocation();
-      }
-    });
-  }
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
+        floatingActionButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                airMapView.centerToUserLocation();
+            }
+        });
+    }
 
-  public void setPeekHeightFirstView(final int peekHeight) {
-    mainBottomSheetBehavior.setPeekHeight(peekHeight);
-  }
+    public void setPeekHeightFirstView(final int peekHeight) {
+        mainBottomSheetBehavior.setPeekHeight(peekHeight);
+    }
 
-  public void setAnchorPoint(final float anchorPoint) {
-    mainBottomSheetBehavior.setAnchorOffset(anchorPoint);
-  }
+    public void setAnchorPoint(final float anchorPoint) {
+        mainBottomSheetBehavior.setAnchorOffset(anchorPoint);
+    }
 
-  public void setBottomSheetStatus(int status) {
-    mainBottomSheetBehavior.setState(status);
-  }
+    public void setBottomSheetStatus(int status) {
+        mainBottomSheetBehavior.setState(status);
+    }
 
-  public void setAirMapView(AirMapView airMapView) {
-    this.airMapView = airMapView;
-  }
+    public void setAirMapView(AirMapView airMapView) {
+        this.airMapView = airMapView;
+    }
 
 }
